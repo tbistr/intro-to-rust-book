fefa2e734a83212b1b72ca01e321d056dbd45af5
jack007xx <jack007xx@gmail.com>
2021-05-15 14:52:59 +0000

    計算機能の追加

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,6 +1,9 @@
+mod rpn_calculator;
+
+use crate::rpn_calculator::RpnCalculator;
 use clap::Clap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -24,12 +27,20 @@ fn main() {
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, opts.verbose);
     } else {
-        println!("No file is specified");
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+// 標準入力でもファイルでも使い回せるように
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
